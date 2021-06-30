63d2c5d49e2a84fdc41af49fd28027e5af914488
ctreactdev <83812404+ctreactdev@users.noreply.github.com>
2021-06-30 02:16:05 +0200

    clean up

--- src/store.ts
+++ src/store.ts
@@ -1,23 +1,3 @@
-// import { combineReducers, createStore } from 'redux'
-// import { devToolsEnhancer } from 'redux-devtools-extension'
-// import { CounterReducer } from './features/counter'
-// import { TodoReducer } from './features/todo'
-
-// /* Create root reducer, containing all features of the application */
-// const rootReducer = combineReducers({
-//   count: CounterReducer,
-//   todos: TodoReducer,
-// })
-
-// const store = createStore(
-//   rootReducer,
-//   /* preloadedState, */ devToolsEnhancer({})
-// )
-
-// export default store
-
-
-
 import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from './features/todo/todoSlice'
 import counterReducer from './features/counter/counterSlice'
