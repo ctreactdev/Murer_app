1705599aa5aa887b02ac9cfd790f6d4d08478ce5
ctreactdev <83812404+ctreactdev@users.noreply.github.com>
2022-05-24 10:25:43 +0200

    WIP: removed slice

--- src/features/counter/counterSlice.tsx
+++ src/features/counter/counterSlice.tsx
@@ -1,22 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    value: 0
+  value: 0,
 }
 
 const counterSlice = createSlice({
-    name: "counter",
-    initialState,
-    reducers: {
-        incrementCount: state => {
-            state.value += 1
-        },
-        decrementCount: state => {
-            state.value -= 1
-        },
-    }
-});
+  name: 'counter',
+  initialState,
+  reducers: {
+    incrementCount: (state) => {
+      state.value += 1
+    },
+    decrementCount: (state) => {
+      state.value -= 1
+    },
+  },
+})
 
 export const { incrementCount, decrementCount } = counterSlice.actions
-export const selectedCount = state => state.counter.value
+export const selectedCount = (state) => state.counter.value
 export default counterSlice.reducer
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import consumptionCalculatorReducer from './features/consumptionCalculator/consumptionCalculatorSlice'
 import counterReducer from './features/counter/counterSlice'
 
 export default configureStore({
   reducer: {
-    consumptionCalculator: consumptionCalculatorReducer,
-    counter: counterReducer
+    counter: counterReducer,
   },
 })
